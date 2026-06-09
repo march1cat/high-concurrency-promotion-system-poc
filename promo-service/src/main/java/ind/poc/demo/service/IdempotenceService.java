0761304c365cfc081d0e255b51e3dd418b4df1d3
march1cat <march1cat@gmail.com>
2026-06-09 17:38:04 +0800

    resolve bug

--- promo-service/src/main/java/ind/poc/demo/service/IdempotenceService.java
+++ promo-service/src/main/java/ind/poc/demo/service/IdempotenceService.java
@@ -13,20 +13,20 @@ public class IdempotenceService<T> {
         Object doneRecord = redisService.get(key);
         if(doneRecord == null) {
             String lockKey = "lock:" + action + ":" + requestId;
-            try {
-                final boolean isLockGetted = redisService.tryLock(lockKey, requestId, 10L);
-                if(isLockGetted){
+            final boolean isLockGetted = redisService.tryLock(lockKey, requestId, 10L);
+            if(isLockGetted){
+                try {
                     doneRecord = redisService.get(key);
                     if(doneRecord == null) {
                         T result = businessLogic.get(); // 執行業務邏輯
-                        redisService.set(key, result, 10L); // 存入結果，下次直接回傳
+                        redisService.set(key, result, 300L); // 存入結果，下次直接回傳
                         doneRecord = result;
                     }
-                } else {
-                    throw new RuntimeException("Duplicated request: " + requestId);
+                } finally {
+                    redisService.releaseLock(lockKey, requestId);
                 }
-            } finally {
-                redisService.releaseLock(lockKey, requestId);
+            } else {
+                throw new RuntimeException("Duplicated request: " + requestId);
             }
         } else {
             return doneRecord;
